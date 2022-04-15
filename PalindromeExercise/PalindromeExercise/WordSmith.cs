a1e640cba5c2f4b0be52233718c6079a4d86ab76
Tylor Moore <mooretylor28@gmail.com>
2022-04-15 12:19:48 -0400

    initial commit

---
+++ PalindromeExercise/PalindromeExercise/WordSmith.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PalindromeExercise
+{
+    public class WordSmith
+    {
+        public WordSmith()
+        {
+        }
+        public bool IsAPalindrome(string word)
+        {
+           // var reversed = "";
+
+           // for (int i = word.Length - 1; i >= 0; i--)
+           // {
+            //    reversed += word.ToLower()[i];
+           // }
+            //return reversed == word.ToLower();
+
+            return word
+                .Replace(" ", " ")
+                .ToLower()
+                .SequenceEqual(word.Replace(" ", " ")
+                .ToLower()
+                .Reverse());
+        }
+    }
+}
--- PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
+++ PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
@@ -1,14 +1,25 @@
 using System;
 using Xunit;
 
-namespace PalindromeExerciseTests
+namespace PalindromeExercise.Tests
 {
     public class UnitTest1
     {
-        [Fact]
-        public void Test1()
+        [Theory]
+        [InlineData("Racecar", true)]
+        [InlineData("hannah", true)]
+        [InlineData("hello", false)]
+        [InlineData("kayak", true)]
+        [InlineData("Tylor", false)]
+        [InlineData("never odd or even", true)]
+        [InlineData("was it a cat i saw", true)]
+        public void PalindromeTester(string word, bool expected)
         {
+            var tester = new WordSmith();
 
+            var acutal = tester.IsAPalindrome(word);
+            
+            Assert.Equal(expected, acutal);
         }
     }
 }
